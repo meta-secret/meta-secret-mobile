dda55cb41fcc51086893e08a5d9a9b832ad22e8a
Dmitry Kuklin <dmitry@meta-secret.com>
2022-10-11 22:28:25 +0300

    Adding lib

---
+++ MetaSecret-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  MetaSecret-Bridging-Header.h
+//  MetaSecret
+//
+//  Created by Dmitry Kuklin on 10.10.2022.
+//
+
+#ifndef MetaSecret_Bridging_Header_h
+#define MetaSecret_Bridging_Header_h
+
+#import "meta-secret-core.h"
+
+#endif /* MetaSecret_Bridging_Header_h */
